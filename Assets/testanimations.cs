025b77fa8b5c26c29ef4ccfb1af0e54683c94eb4
tokyo <aindienomic@gmail.com>
2019-05-11 03:41:17 +0200

    Lets try again

---
+++ Assets/testanimations.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testanimations : MonoBehaviour
+{
+    public Animator anim;
+    bool moving;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.F)){
+            moving = !moving;
+        }
+        anim.SetBool("moving", moving);
+    }
+}
